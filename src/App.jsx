fb2d5d7e9a218fa97227f169c7a704862d6e29e9
Aengus MacKenzie <aengus.2000@gmail.com>
2026-04-17 14:57:38 -0400

    fetch card data from json server in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react"
 import HomePage from "./pages/HomePage";
 import ShopPage from "./pages/ShopPage";
 import AdminPortalPage from "./pages/AdminPortalPage";
 
 function App() {
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/cards")
+      .then((response) => response.json())
+      .then((data) => setCards(data))
+  }, [])
+
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/shop" element={<ShopPage />} />
+      <Route path="/shop" element={<ShopPage cards={cards} />} />
       <Route path="/admin" element={<AdminPortalPage />} />
     </Routes>
   );

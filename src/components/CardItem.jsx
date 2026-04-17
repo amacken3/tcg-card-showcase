861b8819f0b84b23ae367c9ca11de9c85a56b12b
Aengus MacKenzie <aengus.2000@gmail.com>
2026-04-17 16:20:04 -0400

    display full card details in iventory items

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -3,10 +3,18 @@ import EditCardForm from "./EditCardForm";
 function CardItem({ card, onDelete, showDeleteButton = false, updateCard, showEditForm = false }) {
   return (
     <li>
-      {card.name}
+      <h3>{card.name}</h3>
+      <p>Game: {card.game}</p>
+      <p>Set: {card.set}</p>
+      <p>Condition: {card.condition}</p>
+      <p>Price: ${card.price}</p>
+      <p>Stock: {card.stock}</p>
+      {card.image && <img src={card.image} alt={card.name} width="150" />}
+
       {showDeleteButton && (
         <button onClick={() => onDelete(card.id)}>Delete</button>
       )}
+
       {showEditForm && (
         <EditCardForm card={card} updateCard={updateCard} />
       )}
